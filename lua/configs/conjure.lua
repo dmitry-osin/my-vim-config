00ab9b70eab95ac1533f20a0ebf28d76490222b5
Dmitry Osin <daoxsocial@gmail.com>
2024-12-16 17:27:20 +0300

    Add my own nvim configuration based on NvChad

---
+++ lua/configs/conjure.lua
@@ -0,0 +1,6 @@
+vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
+vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
+vim.g["conjure#highlight#enabled"] = true
+vim.g["conjure#log#hud#enabled"] = true
+vim.g["conjure#log#botright"] = true
+vim.g["conjure#log#wrap"] = true
---
+++ lua/plugins/init.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+
+  {
+    "Olical/conjure",
+    ft = {"clojure"},
+    lazy = true,
+    config = function()
+      require("conjure.main").main()
+      require("conjure.mapping")["on-filetype"]()
+    end,
+    dependencies = { "PaterJason/cmp-conjure" },
+  },
+  {
+    "guns/vim-sexp",
+    ft = {"clojure"},
+  },
+  {
+    "tpope/vim-sexp-mappings-for-regular-people",
+    ft = {"clojure"},
+  },
+  {
+    "PaterJason/cmp-conjure",
+    lazy = true,
+    config = function()
+      local cmp = require("cmp")
+      local config = cmp.get_config()
+      table.insert(config.sources, { name = "conjure" })
+      return cmp.setup(config)
+    end
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+}
+}
