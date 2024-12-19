d8ec472d7e427350d0c9c4c5c7a6c3bdc32f07d1
Dmitry Osin <daoxsocial@gmail.com>
2024-12-19 13:22:57 +0300

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.wo.relativenumber = true
+vim.wo.number = true
